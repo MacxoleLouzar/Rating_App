3893bfcfd05f3e952a69ba1cd451b28d0614f94a
DA M1 50_ <dam150_@DA-learners-MacBook-Air-50.local>
2023-06-08 08:54:44 +0200

    Opiwa changes

--- Client/src/Components/RatinForm.jsx
+++ Client/src/Components/RatinForm.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const RatinForm = () => {
     
+
+
   return (
     <div className='Container'>
         <div className="card">
